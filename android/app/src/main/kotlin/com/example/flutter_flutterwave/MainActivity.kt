23b5f27a6bc7664d6ac1d4e7ee7e3c3637daffa7
chidi uchegbu <chidi.uchegbu@hotmail.com>
2025-04-18 02:13:31 +0100

    initial commit

---
+++ android/app/src/main/kotlin/com/example/flutter_flutterwave/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_flutterwave
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_flutterwave/screens/checkout.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutterwave',
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+      ),
+      home: const CheckOutPage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
